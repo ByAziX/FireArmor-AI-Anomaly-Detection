7ac77dd8bd177b50e10f2efc0e6e5b80b7a82dba
Hugo-Azix <60874858+Hugo-Azix@users.noreply.github.com>
2023-05-08 09:50:17 -0500

    Add files via upload

---
+++ FireArmor/AI.py
@@ -0,0 +1,186 @@
+import os
+import psutil
+import pandas as pd
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import random
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import psutil
+import time
+
+
+# étape 1 : collecte des données
+def test_donnees():
+    data = {
+        'pid': [random.randint(1, 10000) for _ in range(100000)],
+        'nom_processus': [f'processus_{random.randint(1, 1000)}' for _ in range(100000)],
+        'temps_creation': [random.uniform(0, 100) for _ in range(100000)],
+        'duration': [random.uniform(0, 100) for _ in range(100000)],
+        'frequency': [random.uniform(0, 100) for _ in range(100000)],
+        'label': [random.choice([0, 1]) for _ in range(100000)]
+    }
+
+    # Créer un DataFrame avec les données générées
+    df = pd.DataFrame(data)
+
+    # Sauvegarder les données dans un fichier CSV
+    df.to_csv('donnees_fictives.csv', index=False)
+
+
+# etape 2 : pretraitement des données
+def pretraitement_donnees():
+    # Charger les données
+    df = pd.read_csv('donnees_fictives.csv')
+
+    # Supprimer les colonnes inutiles
+    df = df.drop(['pid', 'nom_processus'], axis=1)
+
+    # supprimer temps_creation
+    df = df.drop(['temps_creation'], axis=1)
+
+    # Normaliser les données
+
+    # Sauvegarder les données dans un fichier CSV
+    df.to_csv('donnees_fictives.csv', index=False)
+
+# Étape 3: Construction du modèle de logique floue
+
+# Définir des variables linguistiques
+duration = ctrl.Antecedent(np.arange(0, 101, 1), 'duration')
+frequency = ctrl.Antecedent(np.arange(0, 101, 1), 'frequency')
+anomaly = ctrl.Consequent(np.arange(0, 101, 1), 'anomaly')
+
+# Créer des ensembles flous
+duration['low'] = fuzz.trimf(duration.universe, [0, 0, 50])
+duration['medium'] = fuzz.trimf(duration.universe, [0, 50, 100])
+duration['high'] = fuzz.trimf(duration.universe, [50, 100, 100])
+
+frequency['low'] = fuzz.trimf(frequency.universe, [0, 0, 50])
+frequency['medium'] = fuzz.trimf(frequency.universe, [0, 50, 100])
+frequency['high'] = fuzz.trimf(frequency.universe, [50, 100, 100])
+
+anomaly['low'] = fuzz.trimf(anomaly.universe, [0, 0, 50])
+anomaly['medium'] = fuzz.trimf(anomaly.universe, [0, 50, 100])
+anomaly['high'] = fuzz.trimf(anomaly.universe, [50, 100, 100])
+
+# Établir des règles floues
+rule1 = ctrl.Rule(duration['low'] & frequency['low'], anomaly['low'])
+rule2 = ctrl.Rule(duration['medium'] & frequency['low'], anomaly['medium'])
+rule3 = ctrl.Rule(duration['high'] & frequency['low'], anomaly['high'])
+rule4 = ctrl.Rule(duration['low'] & frequency['medium'], anomaly['low'])
+rule5 = ctrl.Rule(duration['medium'] & frequency['medium'], anomaly['medium'])
+rule6 = ctrl.Rule(duration['high'] & frequency['medium'], anomaly['high'])
+rule7 = ctrl.Rule(duration['low'] & frequency['high'], anomaly['low'])
+rule8 = ctrl.Rule(duration['medium'] & frequency['high'], anomaly['medium'])
+rule9 = ctrl.Rule(duration['high'] & frequency['high'], anomaly['high'])
+
+# Créer un système de contrôle flou
+anomaly_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
+anomaly_detection = ctrl.ControlSystemSimulation(anomaly_ctrl)
+
+# Étape 4: Implémentation de l'hystérésis
+def hysteresis(value, lower_threshold, upper_threshold, current_state):
+    if current_state:
+        if value < lower_threshold:
+            return False
+    else:
+        if value > upper_threshold:
+            return True
+    return current_state
+
+current_state = False
+lower_threshold = 30
+upper_threshold = 70
+
+# Étape 5: Entraînement et évaluation du modèle
+# Générer des données aléatoires pour l'entraînement et l'évaluation
+"""data = [{'duration': random.uniform(0, 100), 'frequency': random.uniform(0, 100), 'label': random.choice([0, 1])} for _ in range(1000)]
+df = pd.DataFrame(data)
+df.to_csv('donnees_fictives.csv', index=False)
+"""
+
+# Générer les données
+test_donnees()
+pretraitement_donnees()
+
+# Charger les données
+df = pd.read_csv('donnees_fictives.csv')
+
+# Entraîner et évaluer le modèle
+predictions = []
+for index, row in df.iterrows():
+    anomaly_detection.input['duration'] = row['duration']
+    anomaly_detection.input['frequency'] = row['frequency']
+    anomaly_detection.compute()
+    prediction = 1 if hysteresis(anomaly_detection.output['anomaly'], lower_threshold, upper_threshold, current_state) else 0
+    predictions.append(prediction)
+    current_state = prediction
+
+accuracy = accuracy_score(df['label'], predictions)
+precision = precision_score(df['label'], predictions)
+recall = recall_score(df['label'], predictions)
+f1 = f1_score(df['label'], predictions)
+
+# write prediction to csv
+df['prediction'] = predictions
+df.to_csv('prediction.csv', index=False)
+
+
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+
+
+
+print("-------------------------------------------")
+
+# test real data
+
+desired_pid = 30863
+
+try:
+    
+    # Créer un objet Process avec le PID souhaité
+    pid = psutil.Process()
+
+    process_name = pid.name()
+
+    create_time = pid.create_time()
+
+    create_time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(create_time))
+
+    duration = time.time() - create_time
+
+    pid.cpu_percent()
+
+    time.sleep(1)
+
+    cpu_percent = pid.cpu_percent()
+
+    anomaly_detection.input['duration'] = duration
+    anomaly_detection.input['frequency'] = cpu_percent
+    anomaly_detection.compute()
+    print("Valeur Pour l'hysteresis :",anomaly_detection.output['anomaly'])
+    prediction = 1 if hysteresis(anomaly_detection.output['anomaly'], lower_threshold, upper_threshold, current_state) else 0
+
+    print(f"Process name: {process_name}")
+    print(f"Process creation time: {create_time_str}")
+    print(f"Process duration: {duration} seconds")
+    print(f"Process CPU usage: {cpu_percent}%")
+    print(f"preidction: {prediction} ")
+
+
+except psutil.NoSuchProcess:
+    print(f"Process with PID {desired_pid} does not exist.")
+
+
+
+# Étape 6: Déploiement et mise à jour
+# (La mise en œuvre de cette étape dépend de l'environnement et des exigences spécifiques du projet)
+
+
+# Étape 7: Surveillance et maintenance
+
---
+++ FireArmor/Test/train.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers import LSTM  
+from keras.models import Sequential
+from keras.models import model_from_json
+
+
+import preprocess
+
+# Global hyper-parameters
+sequence_length = 19
+epochs = 1
+batch_size = 50
+feature_dimension = 341
+
+
+def save_model_weight_into_file(model, modelname="model.json", weight="model.h5"):
+    model_json = model.to_json()
+    with open(modelname, "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(weight)
+    print("Saved model to disk in {} and {}".format(modelname,weight))
+
+
+def load_model_and_wieght_from_file(modelname="model.json", weight="model.h5"):
+
+    json_file = open(modelname, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(weight)
+    print("Loaded model from disk, you can do more analysis more")
+
+    pass
+
+def build_model():
+    model = Sequential()
+    layers = {'input': 1, 'hidden1': 64, 'hidden2': 256, 'hidden3': 100, 'output': 1}
+
+    model.add(LSTM(
+            units=layers['hidden1'],
+            input_shape=(sequence_length - 1, layers['input']),
+            return_sequences=True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
+            units=layers['hidden2'],
+            return_sequences=True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
+            units=layers['hidden3'],
+            return_sequences=False))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(
+            units=layers['output']))
+    model.add(Activation("linear"))
+
+    start = time.time()
+    model.compile(loss="mse", optimizer="rmsprop")
+    print("Compilation Time : ", time.time() - start)
+    return model
+
+
+def run_network(model=None, data=None):
+
+    global_start_time = time.time()
+
+    if data is None:
+        print ('Loading data... ')
+        # train on first 700 samples and test on next 300 samples (has anomaly)
+        X_train, y_train  = preprocess.preprocess()
+    else:
+        X_train, y_train = data
+
+    print ("X_train, y_train,shape")
+    print (X_train.shape)
+    print (y_train.shape)
+    print ('\nData Loaded. Compiling...\n')
+
+    if model is None:
+        model = build_model()
+
+    try:
+        print("Training...")
+        model.fit(
+                X_train, y_train,
+                batch_size=batch_size, epochs=epochs, validation_split=0.05)
+        model.summary()
+        print("Predicting...")
+        predicted = model.predict(X_test)
+        print("Reshaping predicted")
+        predicted = np.reshape(predicted, (predicted.size,))
+    except KeyboardInterrupt:
+        print("prediction exception")
+        print ('Training duration (s) : ', time.time() - global_start_time)
+        return model, y_test, 0
+
+    try:
+        plt.figure(1)
+        plt.subplot(311)
+        plt.title("Actual Test Signal w/Anomalies")
+        plt.plot(y_test[:len(y_test)], 'b')
+        plt.subplot(312)
+        plt.title("Predicted Signal")
+        plt.plot(predicted[:len(y_test)], 'g')
+        plt.subplot(313)
+        plt.title("Squared Error")
+        mse = ((y_test - predicted) ** 2)
+        plt.plot(mse, 'r')
+        plt.show()
+    except Exception as e:
+        print("plotting exception")
+        print (str(e))
+    print ('Training duration (s) : ', time.time() - global_start_time)
+
+    return model, y_test, predicted
+
+if __name__ == "__main__":
+    run_network()
