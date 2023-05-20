a3328db8619ea03f764a0b75554fb5f1244e201b
Hugo-Azix <hugo.millet@isen.yncrea.fr>
2023-05-20 06:24:59 -0500

    alémliroation du python pour la dataset

--- FireArmor IA/AI_With_ADFA/ADFA-LD/main.py
+++ FireArmor IA/AI_With_ADFA/ADFA-LD/main.py
@@ -6,17 +6,26 @@ def replace_syscall_with_number(input_file, csv_file, output_file):
     with open(csv_file, 'r') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            syscall_dict[row[0]] = (row[1])
-            print(syscall_dict)
+            if not row or len(row) < 2:  # Skip the row if it is empty or if it doesn't have at least 2 elements
+                continue
+            syscall_name = row[2] 
+            # __NR_ftruncate get the syscall name without __NR_
+            syscall_name = syscall_name.replace('sys_', '').strip()
+            syscall_number = row[1] 
+            syscall_dict[syscall_name] = syscall_number
+    
 
     with open(input_file, 'r') as input_file, open(output_file, 'w') as output_file:
         for line in input_file:
             syscall_name = line.strip()
             if syscall_name in syscall_dict:
                 syscall_number = syscall_dict[syscall_name]
-                output_file.write(f"{syscall_number}\n")
+                output_file.write(syscall_number + '\n')
             else:
-                output_file.write(f"{syscall_name}\n")
+                print('Syscall name not found: ' + syscall_name)
+        
+        
 
 
-replace_syscall_with_number('syscall_names.txt', 'label.csv', 'syscall_num.txt')
+
+replace_syscall_with_number('FireArmor IA/AI_With_ADFA/ADFA-LD/syscall_names.txt', 'FireArmor IA/AI_With_ADFA/ADFA-LD/label.csv', 'FireArmor IA/AI_With_ADFA/ADFA-LD/syscall_num.txt')
